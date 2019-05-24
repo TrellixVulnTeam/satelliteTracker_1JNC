92d9331e5ce0250f2b4629f9dfc9caf88847d92f
Michael Morgan <michael.morgan@sdl.usu.edu>
2019-05-24 16:01:27 -0600

    added additional spacecraft to view, began prep for UI

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,10 @@
 	var vertexColors= THREE.VertexColors
     var scene, camera, renderer, controls, manager; // , stats;
     var windowW = window.innerWidth;
-    var sceneW = window.innerWidth; //      size of the whole screen, /1.8; would set it to be a little bit bigger than a half
+    var sceneW = window.innerWidth / .77; //      size of the whole screen, adding /.8; after innerWidth moves the globe to the right
     var windowH = window.innerHeight;
     var raycaster = new THREE.Raycaster();
-    raycaster.linePrecision = 0.1;
+    raycaster.linePrecision = 0.05;
     var mouse = new THREE.Vector2();
     var parseDate = d3.time.format("%m/%d/%Y").parse;
     var sats = [];
@@ -19,6 +19,17 @@
         mouse.x = (event.clientX / sceneW) * 2 - 1;
         mouse.y = -(event.clientY / windowH) * 2 + 1;
     }
+	
+	window.addEventListener( 'resize', onWindowResize, false );
+
+	function onWindowResize(){
+
+		camera.aspect = window.innerWidth / .77 / window.innerHeight;
+		camera.updateProjectionMatrix();
+
+		renderer.setSize( window.innerWidth / .77, window.innerHeight );
+
+	}
 	
     // Three.js setup procedure
     function setupScene() {
@@ -53,7 +64,7 @@
         //planet.rotateX((-23.4 * Math.PI) / 180); //use this to rotate the globe so the poles are where they are in reality
         var planetMat = new THREE.MeshBasicMaterial({color: 0xffffff});
         var TextureLoader = new THREE.TextureLoader(manager);
-        TextureLoader.load('img/marble.jpg', function (texture) {
+        TextureLoader.load('img/marble3.jpg', function (texture) {
             texture.anisotropy = 8;
             planetMat.map = texture;
             planetMat.needsUpdate = false;
@@ -64,14 +75,14 @@
         var planetMesh = new THREE.Mesh(planet, planetMat);
         planetMesh.add(outlineMesh);
         // displays x, y, and z axes
-        // var axes = new THREE.AxisHelper( 50 );
+        // var axes = new THREE.AxisHelper(50);
         // axes.rotateX((-23.4 * Math.PI)/180);
-        // planetMesh.add( axes );
+        // planetMesh.add(axes);
         scene.add(planetMesh);
     }
 
 	
-	// creates a ground site at a specified latitude and longitude
+	// creates a ground site (right now there are 2) at a specified latitude and longitude
 	function groundSite() {
 		var r = (1.403+6378)*10/6378;
 		var lat = 41.76
@@ -82,22 +93,24 @@
 		var x = -((r) * Math.sin(phi)*Math.cos(theta))
 		var y = ((r) * Math.cos(phi))
 		var z = ((r) * Math.sin(phi)*Math.sin(theta))
+		//creates an elongated yellow cube to show the location of the groundsite
 		var geometry = new THREE.BoxGeometry(.03, .03, 2);
 		var material = new THREE.MeshBasicMaterial({color: 0xffff00});
 		var cube = new THREE.Mesh( geometry, material );
 		cube.position.set(x, y, z);
+		//rotates the cube to radiate out from the center of the globe
 		cube.lookAt(new THREE.Vector3(0, 0, 0));
 		scene.add(cube);
 		
 		r = 10;
-		lat = 28.57
-		lon = -80.65
-		phi   = (90-lat)*(Math.PI/180)
-		theta = (lon+180)*(Math.PI/180)
+		lat = 28.57;
+		lon = -80.65;
+		phi   = (90-lat)*(Math.PI/180);
+		theta = (lon+180)*(Math.PI/180);
 
-		x = -((r) * Math.sin(phi)*Math.cos(theta))
-		y = ((r) * Math.cos(phi))
-		z = ((r) * Math.sin(phi)*Math.sin(theta))
+		x = -((r) * Math.sin(phi)*Math.cos(theta));
+		y = ((r) * Math.cos(phi));
+		z = ((r) * Math.sin(phi)*Math.sin(theta));
 		geometry = new THREE.BoxGeometry(.03, .03, 2);
 		material = new THREE.MeshBasicMaterial({color: 0xffff00});
 		cube = new THREE.Mesh( geometry, material );
@@ -106,7 +119,7 @@
 		scene.add(cube);
 	}
 	
-    // Populate the sats array, calculate orbital elements, put into scene
+    // Populate the sats array, calculate orbital elements, put into scene (not currently being used)
     /*function createSatellites() {
         var color = d3.scale.category20c();
         for (var i = 0; i < sats.length; i++) {
@@ -136,38 +149,52 @@
 	
 	// creates the path of the satellite based on the information in the csv
 	function satPath() {
+		//list of orbital points
 		var OL = 1440;
-		var NS = 7 * OL;
-		var material = new THREE.LineBasicMaterial({color: 0x0000ff});
+		//number of spacecraft to be shown
+		var numCraft = 5;
+		//number of the satellite, starts at 0, used to end an orbit's path and start a new one
+		var iter = 0;
+		//total length of the list of orbital points for the satellites
+		var NS = numCraft * OL;
+		var material = new THREE.LineBasicMaterial({color: 0xffffff, vertexColors: THREE.VertexColors, transparent: true});
 		//https://stackoverflow.com/questions/26790345/vertex-colors-in-three-line
 		// tween.js			https://gist.github.com/vincent/4ce2f9f37b1ac846f84c
 		var geometry = new THREE.Geometry();
-		var group = new THREE.Group();
+		// prev and current are used to determine whether the previous and current orbital points are above the horizon.
 		var prev = 0, current;
 		var r, lat, lon, x, y, z;
-		var satName = newSats[0].name
+		var satName = newSats[0].name;
+		var pathColor = 0x0000ff;
 		for (var i = 0; i < NS; i++) {
+			//if the satellite changes, add the previous satellite's path to the scene and start a new path
 			if (newSats[i].name != satName) {
 				satName = newSats[i].name
+				iter += 1;
 				var line = new THREE.Line( geometry, material );
-				group.add(line);
-				scene.add(group);
-				group = new THREE.Group();
-				var geometry = new THREE.Geometry();
-				var material = new THREE.LineBasicMaterial({color: 0x0000ff});
+				scene.add(line);
+				geometry = new THREE.Geometry();
+				material = new THREE.LineBasicMaterial({color: 0xffffff, vertexColors: THREE.VertexColors, transparent: true});
 			}
 			current = newSats[i].horizon;
-			if (newSats[i].second > 30) {
+			//currently the orbital points are obtained once per minute but the time data isn't always exactly a minute
+			//apart. The following can be used to fix the minutes and seconds so they are each exactly a minute apart.
+			/*if (newSats[i].second > 30) {
 				newSats[i].second = 0;
 				newSats[i].minute += 1;
 			}
 			else {
 				newSats[i].second = 0;
-			}
+			}*/
+			
+			//r = radius of the orbital point
 			r = ((newSats[i].elevation+6378)*10/6378)
+			//lat = satellite latitude
 			lat = newSats[i].latitude
+			//lon = satellite longitute
 			lon = newSats[i].longitude
-			var phi   = (90-lat)*(Math.PI/180)
+			//phi and theta are used to change the keplarian orbital points to an x,y,z format that the globe can use.
+			var phi = (90-lat)*(Math.PI/180)
 			var theta = (lon+180)*(Math.PI/180)
 
 			x = -((r) * Math.sin(phi)*Math.cos(theta))
@@ -175,74 +202,41 @@
 			y = ((r) * Math.cos(phi))
 			// if the satellite path becomes visible to the groundsite's position, change the path color to red
 			if (current == 1 && prev == 0) {
+				pathColor = 0xff0000;
+				geometry.colors[i - (OL*iter)] = new THREE.Color(pathColor);
 				geometry.vertices.push(new THREE.Vector3(x, y, z));
-				var line = new THREE.Line( geometry, material );
+				
 				//line.rotateX((-23.4 * Math.PI) / 180); //use this if the globe is rotated to show the true position of the poles
-				group.add( line );
-				var material = new THREE.LineBasicMaterial({color: 0xff0000});
-				var geometry = new THREE.Geometry();
-				geometry.vertices.push(new THREE.Vector3( x, y, z));
 			}
-			// if we transition from a visible path to not being in line of site of the satellite, change back to a blue path
+			// if the satellite path lowers below the ground site's horizon, change the path color back to blue
 			else if (current == 0 && prev == 1) {
-				geometry.vertices.push(new THREE.Vector3( x, y, z));
-				var line = new THREE.Line(geometry, material);
+				pathColor = 0x0000ff;
+				geometry.colors[i - (OL*iter)] = new THREE.Color(pathColor);
 				//line.rotateX((-23.4 * Math.PI) / 180); //use this if the globe is rotated to show the true position of the poles
-				group.add(line);
-				var material = new THREE.LineBasicMaterial({color: 0x0000ff});
-				var geometry = new THREE.Geometry();
 				geometry.vertices.push(new THREE.Vector3(x, y, z));
+				
 			}
+			// otherwise, the color of the path should stay what the <if> or <else if> changed it to.
 			else {
+				geometry.colors[i - (OL*iter)] = new THREE.Color(pathColor);
 				geometry.vertices.push(new THREE.Vector3(x, y, z));
+				
 			}
 			prev = current;
 		}
 		var line = new THREE.Line(geometry, material);
-		group.add(line);
 		//line.rotateX((-23.4 * Math.PI) / 180); //use this if the globe is rotated to show the true position of the poles
-		scene.add(group);	
+		scene.add(line);	
 	}
 	
-    // Draw a line along the Y axis (shows the poles)
+    // Draw a green line along the Y axis (shows the poles) (not currently being used)
     function createDistanceLine() {
-        var lineMat = new THREE.LineBasicMaterial({
-            color: 0xFFC400
-        });
-        var distanceGeo = new THREE.Geometry();
-        distanceGeo.vertices.push(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 15, 0));
-		distanceGeo.vertices.push(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, -15, 0));
-        var line = new THREE.Line(distanceGeo, lineMat);
+        var poleMat = new THREE.LineBasicMaterial({color: 0x00ff00});
+        var poleGeo = new THREE.Geometry();
+        poleGeo.vertices.push(new THREE.Vector3(0, -15, 0), new THREE.Vector3(0, 15, 0));
+        var line = new THREE.Line(poleGeo, poleMat);
 		//line.rotateX((-23.4 * Math.PI) / 180); //use this if the globe is rotated to show the true position of the poles
         scene.add(line);
-    }
-    
-    function addTag(property, name) {
-        d3.select(".tags")
-            .append("div")
-            .classed("tag", true)
-            .style("float", "left")
-            .style("padding", 3 + "px")
-            .style("margin", 1 + "px")
-            .style("background-color", "white")
-            .style("cursor", "pointer")
-            .html(property + ": " + name);
-        d3.select(".reset")
-            .style("opacity", 1)
-            .on('mouseenter', function () {
-            d3.select(this).style("color", "#ff0000");
-        })
-            .on('mouseleave', function () {
-            d3.select(this).style("color", "#ffffff");
-        })
-            .on('click', function () {
-            d3.selectAll(".tag").remove();
-            sats.forEach(function (e) {
-                e.mat.visible = true;
-            });
-            d3.select("body").selectAll("svg").selectAll("path").classed("noClick", false);
-            d3.select(".reset").style("opacity", 0.1);
-        });
     }
 	
     function init() {
@@ -295,7 +289,7 @@
         controls.update();
         //stats.begin();
         if (mouse.x < sceneW) {
-            //checkForRaycasts();
+            checkForRaycasts();
         }
         renderer.render(scene, camera);
         //stats.end();
