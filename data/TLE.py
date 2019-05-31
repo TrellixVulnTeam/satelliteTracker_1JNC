2dfbe266b28f30372fb3e90fda8b43eed7486388
Michael Morgan <michael.morgan@sdl.usu.edu>
2019-05-31 16:04:46 -0600

    moved javascript to one file, working on updating color of satellite paths depending on active groundsites

--- data/TLE.py
+++ data/TLE.py
@@ -4,17 +4,16 @@ from spacetrack import SpaceTrackClient
 import spacetrack.operators as op
 from numpy import diff
 import numpy as np
-# pip install skyfield    https://rhodesmill.org/skyfield/
+# pip install skyfield	  https://rhodesmill.org/skyfield/
 from skyfield.api import Topos, load
 import json
-# https://pynative.com/python-mysql-database-connection/    https://pynative.com/install-mysql-connector-python/
+# https://pynative.com/python-mysql-database-connection/	https://pynative.com/install-mysql-connector-python/
 # pip install mysql-connector-python
 # import mysql.connector
 from datetime import datetime, timedelta
 from pytz import timezone
 
-# some ideas for going forward:
-# Django: https://medium.com/cs-math/11-things-i-wish-i-knew-about-django-development-before-i-started-my-company-f29f6080c131
+
 
 
 # get TLE information from CelesTrakstations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
@@ -29,51 +28,60 @@ IDs = [4, 8, 9, 11, 12, 18, 19, 20, 22, 24, 25, 26, 29, 30, 31, 32, 33, 34, 46, 
 43021, 43231, 43466, 43467, 43468, 43546, 43547, 43548, 43549, 43550, 43551, 43552, 43553, 43554, 43556, 43557, 43558,
 43559, 43560, 43561, 43565, 43595, 43596, 43597, 43598, 43638, 43639, 43640, 43702, 43756, 43870, 44029, 44030, 44031,
 44032, 44033, 44069, 44110, 44188, 44222, 44229]
+
+groundSites = {"NASA HQ": Topos('38.883056 N', '-77.017369 E'), "NASA MCC": Topos('29.557857 N', '-95.089023 E'),
+"Kennedy Space Center": Topos('28.579680 N', '-80.653010 E'), "Moscow MCC": Topos('55.912104 N', '37.810254 E'),
+"Baikonur Cosmodrome": Topos('45.963929 N', '63.305125 E'), "Canadian Space Center": Topos('45.521186 N', '-73.393632 E'),
+"German Space Op Center": Topos('48.086873 N', '11.280641 E'), "BIOTESC": Topos('46.994580 N', '8.310018 E'),
+"Guiana Space Center": Topos('5.224441 N', '-52.776433 E'), "Tsukuba Space Center": Topos('36.065140 N', '140.127613 E')}
+horizonData = []
+
+
 timeNow = datetime.now()
 fl = 'time.txt'
 with open(fl, 'r') as file:
-    timeOld = file.read()
-    timeOld = datetime.strptime(timeOld, '%Y-%m-%d %H:%M:%S.%f')
+	timeOld = file.read()
+	timeOld = datetime.strptime(timeOld, '%Y-%m-%d %H:%M:%S.%f')
 
 # gets new data from space-track.org if it has been longer than a day
 # that TLE data has been requested
 if timeNow > timeOld:
-    # TODO: validate username and password with spacetrack API
-    user = input("spacetrack username: ")
-    pd = input("password: ")
-    st = SpaceTrackClient(user, pd)
-    timeOld = datetime(timeNow.year, timeNow.month, timeNow.day, 0, 7, 35) + timedelta(days=1)
-    timeOld += timedelta(seconds=.3333333)
-    with open(fl, 'w') as file:
-        file.write(str(timeOld))
-    # norad_cat_id=[op.inclusive_range(1,36050)] instead of norad_cat_id=[25544, 42712], etc.
-    data = st.tle_latest(norad_cat_id=IDs, ordinal=1, format='json')
-    a = json.loads(data)
-    print("got new data")
-    spacecraftNames.clear()
+	# TODO: validate username and password with spacetrack API
+	user = input("spacetrack username: ")
+	pd = input("password: ")
+	st = SpaceTrackClient(user, pd)
+	timeOld = datetime(timeNow.year, timeNow.month, timeNow.day, 0, 7, 35) + timedelta(days=1)
+	timeOld += timedelta(seconds=.3333333)
+	with open(fl, 'w') as file:
+		file.write(str(timeOld))
+	# norad_cat_id=[op.inclusive_range(1,36050)] instead of norad_cat_id=[25544, 42712], etc.
+	data = st.tle_latest(norad_cat_id=IDs, ordinal=1, format='json')
+	a = json.loads(data)
+	print("got new data")
+	spacecraftNames.clear()
 
-    fl = 'spacecraft.txt'
-    with open(fl, 'w') as file:
-        for x in range(len(a)):
-            craft = a[x]
-            b = craft['TLE_LINE0'] + '\n'
-            file.write(b)
-            name = b[2:]
-            spacecraftNames.append(name)
-            b = craft['TLE_LINE1'] + '\n'
-            file.write(b)
-            b = craft['TLE_LINE2'] + '\n'
-            file.write(b)
-    fl = 'names.txt'
-    with open(fl, 'w') as file:
-        for name in spacecraftNames:
-            file.write(name)
+	fl = 'spacecraft.txt'
+	with open(fl, 'w') as file:
+		for x in range(len(a)):
+			craft = a[x]
+			b = craft['TLE_LINE0'] + '\n'
+			file.write(b)
+			name = b[2:]
+			spacecraftNames.append(name)
+			b = craft['TLE_LINE1'] + '\n'
+			file.write(b)
+			b = craft['TLE_LINE2'] + '\n'
+			file.write(b)
+	fl = 'names.txt'
+	with open(fl, 'w') as file:
+		for name in spacecraftNames:
+			file.write(name)
 
 if len(spacecraftNames) == 0:
-    fl = 'names.txt'
-    with open(fl, 'r') as file:
-        for line in file:
-            spacecraftNames.append(line)
+	fl = 'names.txt'
+	with open(fl, 'r') as file:
+		for line in file:
+			spacecraftNames.append(line)
 
 fl = 'spacecraft.txt'
 # loads a dictionary of spacecraft TLEs and other relevant information
@@ -84,77 +92,72 @@ fl = "position.csv"
 f = open(fl, "w+")
 f.close()
 with open(fl, 'a') as file:
-    file.write('name,')
-    file.write('latitude,')
-    file.write('longitude,')
-    file.write('elevation,')
-    file.write('year,')
-    file.write('month,')
-    file.write('day,')
-    file.write('hour,')
-    file.write('minute,')
-    file.write('second,')
-    file.write('horizon,\n')
+	file.write('name,')
+	file.write('latitude,')
+	file.write('longitude,')
+	file.write('elevation,')
+	file.write('year,')
+	file.write('month,')
+	file.write('day,')
+	file.write('hour,')
+	file.write('minute,')
+	file.write('second,')
+	for i in range(len(groundSites)):
+		a = i+1;
+		file.write('h'+str(a) + ',')
+	file.write(',\n')
 ts = load.timescale()
-ground = Topos('41.72 N', '-111.82 E', None, None, 1403)
-ground2 = Topos('28.57 N', '-80.65 E')
 # creates a list of every minute for the next 24 hours
 minutes = range(60*24)
 # creates a list of times for each minute of the above minutes list
 t = ts.utc(d.year, d.month, d.day, d.hour + (d.minute / 60), minutes)
 mountain = timezone('US/Mountain')
-timetemp1 = datetime.now();
 for craft in spacecraftNames:
-    # grabs a spacecraft by its name from the dictionary, removing the \n
-    # character at the end of the spacecraftNames string
-    satellite = satellites[craft[:-1]]
+	# grabs a spacecraft by its name from the dictionary, removing the \n
+	# character at the end of the spacecraftNames string
+	satellite = satellites[craft[:-1]]
 
-    """"# specifies the ground site latitude, longitude and elevation
-    t = ts.now()
-    difference = satellite - ground
-    topocentric = difference.at(t)
-    # the following five lines were an experiment to test whether the
-    # spacecraft was currently above the horizon of the ground site or not
-    alt, az, distance = topocentric.altaz()
-    if alt.degrees > 0:
-        print(spacecraftNames[0][:-1], 'is above the horizon')
-    else:
-        print(spacecraftNames[0][:-1], 'is not above the horizon')
-    """
-    # https://rhodesmill.org/brandon/2018/tiangong/
+	""""# specifies the ground site latitude, longitude and elevation
+	t = ts.now()
+	difference = satellite - ground
+	topocentric = difference.at(t)
+	# the following five lines were an experiment to test whether the
+	# spacecraft was currently above the horizon of the ground site or not
+	alt, az, distance = topocentric.altaz()
+	if alt.degrees > 0:
+		print(spacecraftNames[0][:-1], 'is above the horizon')
+	else:
+		print(spacecraftNames[0][:-1], 'is not above the horizon')
+	"""
+	# https://rhodesmill.org/brandon/2018/tiangong/
 
-    beginning = datetime.now()
-    # gets the spacecraft position at time t, which is a list in this case
-    geocentric = satellite.at(t)
-    # gets the longitude latitude of the point on the earth below the spacecraft,
-    # and the spacecraft's elevation above the earth at time t
-    subpoint = geocentric.subpoint()
-    lat = subpoint.latitude.degrees
-    long = subpoint.longitude.degrees
-    el = subpoint.elevation.km
-    # calculates the positions of the spacecraft during the list of times in t
-    # to get an orbit
-    orbit = (satellite - ground).at(t)
-    orbit2 = (satellite - ground2).at(t)
-    alt, az, dist = orbit.altaz()
-    alt2, az2, dist2 = orbit2.altaz()
-    above_horizon1 = np.array(alt.degrees > 0)
-    above_horizon2 = np.array(alt2.degrees > 0)
-    above_horizon = above_horizon1 + above_horizon2
+	beginning = datetime.now()
+	# gets the spacecraft position at time t, which is a list in this case
+	geocentric = satellite.at(t)
+	# gets the longitude latitude of the point on the earth below the spacecraft,
+	# and the spacecraft's elevation above the earth at time t
+	subpoint = geocentric.subpoint()
+	lat = subpoint.latitude.degrees
+	long = subpoint.longitude.degrees
+	el = subpoint.elevation.km
+	# calculates the positions of the spacecraft during the list of times in t
+	# to get an orbit
+	for i in groundSites:
+		orbit = (satellite - groundSites[i]).at(t)
+		alt, az, dist = orbit.altaz()
+		horizonData.append(np.array(alt.degrees > 0))
 
-    # gets data about the spacecraft and its orbit and writes it to a csv file
-    with open(fl, 'a') as file:
-        for i in range(len(lat)):
-            file.write(craft[:-1] + ',')
-            file.write(str(lat[i]) + ',')
-            file.write(str(long[i]) + ',')
-            file.write(str(el[i]) + ',')
-            temp = str(t[i].utc)[1:-1]
-            file.write(temp + ',')
-            file.write(str(int(above_horizon[i])))
-            file.write(',\n')
-    ending = datetime.now()
-    print(alt, 'calculated in', ending - beginning, 'seconds')
-
-timetemp2 = datetime.now()
-print("finished in",timetemp2-timetemp1,"seconds")
+	# gets data about the spacecraft and its orbit and writes it to a csv file
+	with open(fl, 'a') as file:
+		for i in range(len(lat)):
+			file.write(craft[:-1] + ',')
+			file.write(str(lat[i]) + ',')
+			file.write(str(long[i]) + ',')
+			file.write(str(el[i]) + ',')
+			temp = str(t[i].utc)[1:-1]
+			file.write(temp + ',')
+			for j in range(len(groundSites)):
+				file.write(str(int(horizonData[j][i])) + ',')
+			file.write(',\n')
+	ending = datetime.now()
+	print(alt, 'calculated in', ending - beginning, 'seconds')
--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ tab for instructions/explanation:
 	https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_tabs_fade
 
 show satellite current position, along with updated positions as time goes on
+// tween.js			https://gist.github.com/vincent/4ce2f9f37b1ac846f84c
 
 add/remove groundsites. For adding, I may be able to just hold the orbital data in
 python and just rerun the above_horizon on them based on the new groundsite information
@@ -28,6 +29,27 @@ the 1s from that groundsite's array to the final array. This could speed things 
     var mouse = new THREE.Vector2();
     var parseDate = d3.time.format("%m/%d/%Y").parse;
     window.groundSites = {};
+	var horizonArr = [];
+	var groundData = [[],[],[],[],[],[],[],[],[],[]];
+	var spacecraft = ["1KUNS-PF", "AEROCUBE 12A", "AEROCUBE 12B", "AGILE", "ALTAIR PATHFINDER", "AQUA", 
+	"ASTERIA", "BANXING-2", "CATSAT-1", "CATSAT-2", "CUBERRT", "CYGNUS NG-11", "DELLINGR (RBLE)", "DELPHINI", 
+	"DISCOVERER 11", "DISCOVERER 13", "DISCOVERER 5", "DISCOVERER 5 CAPSULE", "DISCOVERER 6", "DISCOVERER 7", 
+	"DISCOVERER 8", "DRAGON CRS-17", "ECHO 1", "EQUISAT", "EXOS D (AKEBONO)", "EXPLORER 1", "EXPLORER 4", "EXPLORER 7", 
+	"EXPRESS AM-44", "FUNCUBE 1", "GALILEO 26 (2C2)", "GEOTAIL", "GLOBALSTAR M096", "HALOSAT", "HARBINGER", 
+	"HST", "IBEX", "INTEGRAL", "IRAZU", "ISS (ZARYA)", "ISS DEB (SEDA-AP)", "KESTREL EYE 2M", "KMS 4", 
+	"LANDSAT 8", "LEMUR 2 ALEXANDER", "LEMUR 2 TOMHENDERSON", "LEMUR 2 VU", "LEMUR 2 YUASA", "METEOR M2", 
+	"MMS 1", "NAVSTAR 75 (USA 265)", "NJUST-1", "NOAA 19", "NSIGHT-1", "O3B FM15", "OBJECT NZ", "OBJECT PB", 
+	"OBJECT PC", "OBJECT PG", "OBJECT PH", "OBJECT PJ", "OBJECT PK", "OBJECT PN", "OBJECT PP", "PRISMA (TANGO)", 
+	"PROGRESS MS-10", "PROGRESS MS-11", "RADIX", "RAINCUBE", "SAUDISAT 1C", "SES 15", "SIRIUS 3", 
+	"SOLRAD 1 (GREB)", "SOYUZ MS-11", "SOYUZ MS-12", "SPEKTR R", "SPUTNIK 3", "SPUTNIK 4", "STARS-ME", 
+	"TACSAT 4", "TECHEDSAT 8", "TEMPEST-D", "TERRA", "THEMIS A", "THEMIS D", "THOR ABLESTAR DEB", 
+	"THOR ABLESTAR R/B", "TIANGONG-2", "TIROS 1", "TRANSIT 1B", "UBAKUSAT", "UNITE", "VANGUARD 2", 
+	"VANGUARD 3", "VANGUARD R/B", "VIASAT 1", "VIASAT 2", "VOSTOK 2", "XMM", "ZA-AEROSAT"];
+	var sites = ["NASA HQ","NASA Mission Control Center","Kennedy Space Center","Moscow Mission Control Center",
+	"Baikonur Cosmodrome","Canadian Space Center","German Space Operation Center","BIOTESC (Zurich)",
+	"Guiana Space Center","Tsukuba Space Center (JAXA)"];
+	var gsLat = [38.883056,29.557857,28.579680,55.912104,45.963929,45.521186,48.086873,46.994580,5.224441,36.065140];
+	var gsLon = [-77.017369,-95.089023,-80.653010,37.810254,63.305125,-73.393632,11.280641,8.310018,-52.776433,140.127613];
 	window.satDict = {};
 	var rawSatData = []; //the array holding all the satellite data after parsing csv
     function onMouseMove(event) {
@@ -37,7 +59,149 @@ the 1s from that groundsite's array to the final array. This could speed things 
         mouse.y = -(event.clientY / windowH) * 2 + 1;
     }
 	
+	function openSideBar() {
+		setTimeout(function(){
+		document.getElementById("gsTitle").style.width = "20%";
+		document.getElementById("gsTitle").style.height = "20%"
+		document.getElementById("gsTitle").style.top = "0%";
+		document.getElementById("gsTitle").visible = true;
+		document.getElementById("gsnav").style.width = "20%";
+		document.getElementById("gsnav").style.height = "20%"
+		document.getElementById("gsnav").style.top = "17%";
+		document.getElementById("gsnav").visible = true;
+		document.getElementById("spacecraftTitle").style.width = "20%";
+		document.getElementById("spacecraftTitle").style.height = "20%"
+		document.getElementById("spacecraftTitle").style.top = "37%";
+		document.getElementById("spacecraftTitle").visible = true;
+		document.getElementById("craftnav").style.width = "20%";
+		document.getElementById("craftnav").style.height = "46%";
+		document.getElementById("craftnav").style.top = "54%";
+		document.getElementById("craftnav").visible = true;
+		}, 500);
+	}
+	
 	window.addEventListener( 'resize', onWindowResize, false );
+	
+	document.querySelector('#gsAll').onclick = function (ev) {
+		checkAll(ev.target.checked, 'gsCheck');
+	}
+	document.querySelector('#listgs').onclick = function (ev) {
+		checkClick(ev.target.name,ev.target.checked,'gsCheck');
+	}
+	document.querySelector('#allCheck').onclick = function (ev) {
+		checkAll(ev.target.checked, 'spacecraftCheck');
+	}
+	document.querySelector('#listCraft').onclick = function (ev) {
+		checkClick(ev.target.name,ev.target.checked,'spacecraftCheck');
+	}
+	
+	function addVisiblePath(checkboxes) {
+		//TODO add function to change the color of the part of each path that is visible to the active groundsites
+		for (var i = 0; i < 14400; i++) {
+			var totalVisible = 0;
+			for (var j = 0; j < checkboxes.length; j++) {
+				if (checkboxes[j].checked) {
+					totalVisible += groundData[j][i];
+				}
+			}
+			horizonArr.push(totalVisible);
+		}
+	}
+	
+	function checkAll(check, cl) {
+		var checkboxes = document.getElementsByClassName(cl);
+		for (var i = 0; i < checkboxes.length; i++) {
+			if (checkboxes[i].className == 'spacecraftCheck') {
+				if (check) {
+					checkboxes[i].checked = true;
+					satDict[checkboxes[i].name].visible = true;
+				}
+				else {
+					checkboxes[i].checked = false;
+					satDict[checkboxes[i].name].visible = false;
+				}
+			}
+			else {
+				if (check) {
+					checkboxes[i].checked = true;
+					groundSites[checkboxes[i].name].visible = true;
+				}
+				else {
+					checkboxes[i].checked = false;
+					groundSites[checkboxes[i].name].visible = false;
+				}
+			}
+		}
+		if (cl == 'gsCheck') {
+			addVisiblePath(checkboxes);
+		}
+	}
+	
+	function checkClick(name, check, cl) {
+		var allButton;
+		if (cl == 'spacecraftCheck') {
+			var sat = satDict[name];
+			if (check) {
+				sat.visible = true;
+			}
+			else {
+				sat.visible = false;
+			}
+			allButton = 'allCheck';
+		}
+		else {
+			var gs = groundSites[name];
+			if (check) {
+				gs.visible = true;
+			}
+			else {
+				gs.visible = false;
+			}
+			allButton = 'gsAll';
+		}
+		var checkboxes = document.getElementsByClassName(cl);
+		
+		checkForAllChecked(checkboxes,allButton,cl);
+	}
+
+	function checkForAllChecked(checkboxes, buttonName, cl) {
+		var numChecked = 0;
+		var allButton = document.getElementById(buttonName);
+		for (var i = 0; i < checkboxes.length; i++) {
+			if (checkboxes[i].className == cl) {
+				if (checkboxes[i].checked == true) {
+					numChecked += 1;
+				}
+			}
+		}
+		if (numChecked == checkboxes.length) {
+			allButton.checked = true;
+		}
+		else {
+			allButton.checked = false;
+		}
+		if (cl == 'gsCheck') {
+			addVisiblePath(checkboxes);
+		}
+	}
+	
+	function craftList() {
+		var text = "";
+		var i;
+		for (i = 0; i < spacecraft.length; i++) {
+			text += "<label>" + spacecraft[i] + "<input type=" + "\"" + "checkbox" + "\"" + " id=" + "\"" + spacecraft[i] + "\"" + "class=" + "\"" + "spacecraftCheck" + "\"" + " name =" + "\"" + spacecraft[i] + "\"" + "></label><hr>"
+		}
+		document.getElementById("listCraft").innerHTML = text;
+	}
+	
+	function gsList() {
+		var text = "";
+		var i;
+		for (i = 0; i < sites.length; i++) {
+			text += "<label>" + sites[i] + "<input type=" + "\"" + "checkbox" + "\"" + " id=" + "\"" + sites[i] + "\"" + "class=" + "\"" + "gsCheck" + "\"" + " name =" + "\"" + sites[i] + "\"" + "></label><hr>"
+		}
+		document.getElementById("listgs").innerHTML = text;
+	}
 	
     // Three.js setup procedure
     function setupScene() {
@@ -50,8 +214,10 @@ the 1s from that groundsite's array to the final array. This could speed things 
         camera.position.z = 22;
 		camera.position.y = 13;
         renderer.shadowMap.enabled = false;
+		gsList();
+		craftList();
         manager = new THREE.LoadingManager();
-        manager.onLoad = function () {render();};
+        manager.onLoad = function () {openSideBar();render();};
     }
 	
 	function onWindowResize(){
@@ -88,54 +254,44 @@ the 1s from that groundsite's array to the final array. This could speed things 
         outlineMesh.scale.multiplyScalar(1.004);
         var planetMesh = new THREE.Mesh(planet, planetMat);
         planetMesh.add(outlineMesh);
-        // displays x, y, and z axes
-        // var axes = new THREE.AxisHelper(50);
-        // axes.rotateX((-23.4 * Math.PI)/180);
-        // planetMesh.add(axes);
         scene.add(planetMesh);
     }
 	
 	// creates a ground site (right now there are 2) at a specified latitude and longitude
 	function groundSite() {
-		var r = (1.403+6378)*10/6378;
-		var lat = 41.76;
-		var lon = -111.82;
-		var phi   = (90-lat)*(Math.PI/180);
-		var theta = (lon+180)*(Math.PI/180);
-
-		var x = -((r) * Math.sin(phi)*Math.cos(theta));
-		var y = ((r) * Math.cos(phi));
-		var z = ((r) * Math.sin(phi)*Math.sin(theta));
-		//creates an elongated yellow cube to show the location of the groundsite
-		var geometry = new THREE.BoxGeometry(.03, .03, 2);
-		var material = new THREE.MeshBasicMaterial({color: 0xffff00});
-		var cube = new THREE.Mesh(geometry, material);
-		cube.position.set(x, y, z);
-		//rotates the cube to radiate out from the center of the globe
-		cube.lookAt(new THREE.Vector3(0, 0, 0));
-		cube.name = "test1";
-		groundSites[cube.name] = cube;
-		cube.visible = false;
-		scene.add(cube);
-		
-		r = 10;
-		lat = 28.57;
-		lon = -80.65;
-		phi = (90-lat)*(Math.PI/180);
-		theta = (lon+180)*(Math.PI/180);
-
-		x = -((r) * Math.sin(phi)*Math.cos(theta));
-		y = ((r) * Math.cos(phi));
-		z = ((r) * Math.sin(phi)*Math.sin(theta));
-		geometry = new THREE.BoxGeometry(.03, .03, 2);
-		material = new THREE.MeshBasicMaterial({color: 0xffff00});
-		cube = new THREE.Mesh(geometry, material);
-		cube.position.set(x, y, z);
-		cube.lookAt(new THREE.Vector3(0, 0, 0));
-		cube.name = "test2";
-		groundSites[cube.name] = cube;
-		cube.visible = false;
-		scene.add(cube);
+		var r = 10;
+		for (var i = 0; i < gsLat.length; i++) {
+			var lat = gsLat[i];
+			var lon = gsLon[i];
+			var phi   = (90-lat)*(Math.PI/180);
+			var theta = (lon+180)*(Math.PI/180);
+			var x = -((r) * Math.sin(phi)*Math.cos(theta));
+			var y = ((r) * Math.cos(phi));
+			var z = ((r) * Math.sin(phi)*Math.sin(theta));
+			var geometry = new THREE.BoxGeometry(.03, .03, 2);
+			var material = new THREE.MeshBasicMaterial({color: 0xffff00});
+			var cube = new THREE.Mesh(geometry, material);
+			//creates an elongated yellow cube to show the location of the groundsite
+			cube.position.set(x, y, z);
+			//rotates the cube to radiate out from the center of the globe
+			cube.lookAt(new THREE.Vector3(0, 0, 0));
+			cube.visible = false;
+			var gName = sites[i];
+			groundSites[gName] = cube;			
+			scene.add(cube);
+		}
+		for (var i = 0; i < 144000; i++) {
+			groundData[0].push(rawSatData[i].h1);
+			groundData[1].push(rawSatData[i].h2);
+			groundData[2].push(rawSatData[i].h3);
+			groundData[3].push(rawSatData[i].h4);
+			groundData[4].push(rawSatData[i].h5);
+			groundData[5].push(rawSatData[i].h6);
+			groundData[6].push(rawSatData[i].h7);
+			groundData[7].push(rawSatData[i].h8);
+			groundData[8].push(rawSatData[i].h9);
+			groundData[9].push(rawSatData[i].h10);
+		}
 	}
 	
 	// creates the path of the satellite based on the information in the csv
@@ -149,7 +305,6 @@ the 1s from that groundsite's array to the final array. This could speed things 
 		//total length of the list of orbital points for the satellites
 		var NS = numCraft * OL;
 		var material = new THREE.LineBasicMaterial({color: 0xffffff, vertexColors: THREE.VertexColors, transparent: true});
-		// tween.js			https://gist.github.com/vincent/4ce2f9f37b1ac846f84c
 		var geometry = new THREE.Geometry();
 		// prev and current are used to determine whether the previous and current orbital points are above the horizon.
 		var prev = 0, current;
@@ -170,15 +325,14 @@ the 1s from that groundsite's array to the final array. This could speed things 
 				material = new THREE.LineBasicMaterial({color: 0xffffff, vertexColors: THREE.VertexColors, transparent: true});
 			}
 			current = rawSatData[i].horizon;
-			//currently the orbital points are obtained once per minute but the time data isn't always exactly a minute
-			//apart. The following can be used to fix the minutes and seconds so they are each exactly a minute apart.
-			/*if (rawSatData[i].second > 30) {
+			//some of the seconds are off in the data sent from the python application. This rectifies the issue
+			if (rawSatData[i].second > 30) {
 				rawSatData[i].second = 0;
 				rawSatData[i].minute += 1;
 			}
 			else {
 				rawSatData[i].second = 0;
-			}*/
+			}
 			
 			//r = radius of the orbital point
 			r = ((rawSatData[i].elevation+6378)*10/6378);
@@ -290,11 +444,21 @@ the 1s from that groundsite's array to the final array. This could speed things 
 			hour: +d.hour,
 			minute: +d.minute,
 			second: +d.second,
-            horizon: d.horizon
+            h1: d.h1,
+			h2: d.h2,
+			h3: d.h3,
+			h4: d.h4,
+			h5: d.h5,
+			h6: d.h6,
+			h7: d.h7,
+			h8: d.h8,
+			h9: d.h9,
+			h10: d.h10
         };
     }, function (data) {
         rawSatData = data.slice(); //copy 
 		init();
     });
+	
 
 })(window, document);
