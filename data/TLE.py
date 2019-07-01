bff73f17f5cd75ee51739dee95c06432b2e68084
Michael Morgan <michael.morgan@sdl.usu.edu>
2019-07-01 13:25:59 -0600

    further refactoring of TLE.py

--- data/TLE.py
+++ data/TLE.py
@@ -6,15 +6,14 @@ from numpy import diff
 import numpy as np
 # pip install skyfield	  https://rhodesmill.org/skyfield/
 from skyfield.api import Topos, load
-from skyfield import almanac
 import json
 # https://pynative.com/python-mysql-database-connection/	https://pynative.com/install-mysql-connector-python/
 # pip install mysql-connector-python
 # import mysql.connector
 from datetime import datetime, timedelta
-from pytz import timezone
-import threading
-from multiprocessing.dummy import Pool as ThreadPool
+from numba import jit
+
+
 
 # TODO: add a < while True: > loop to the program that checks for the times when the satellite data
 # be updated. It will need to check right after we pass hour = 0, minute = 7 and second = 36 to grab
@@ -51,8 +50,15 @@ siteNames = np.array(
      "Troll Satellite Station (Antarctica)", "Canberra Deep Space Complex (Australia)",
      "KSAT Hartebeesthoek (South Africa)", "North Pole Satellite Station (Alaska)", "Master Control Facility (India)"])
 
-
-
+iter = 0
+ts = load.timescale()
+m = 60
+hr = 6
+minutes = range(m * hr)
+d = datetime.utcnow()
+t = ts.utc(d.year, d.month, d.day, d.hour + (d.minute / 60), minutes)
+t.gast = t.gmst
+fl = 'satelliteData.csv'
 
 
 # potential help to speed up calculations:
@@ -65,31 +71,28 @@ siteNames = np.array(
 def sortNames(e):
     return e.name
 
-def compareTime():
-    timeNow = datetime.now()
+def compareTime(timeNow):
     fl = 'time.txt'
     with open(fl, 'r') as file:
         timeOld = file.read()
         timeOld = datetime.strptime(timeOld, '%Y-%m-%d %H:%M:%S.%f')
     if timeNow > timeOld:
-        getNewData(timeNow)
-    else:
-        fl = 'names.txt'
-        with open(fl, 'r') as file:
-            for line in file:
-                spacecraftNames.append(line)
+        getNewData()
+        timeOld = datetime(timeNow.year, timeNow.month, timeNow.day, 0, 7, 35) + timedelta(days=1)
+        timeOld += timedelta(seconds=.3333333)
+        with open(fl, 'w') as file:
+            file.write(str(timeOld))
     fl = 'spacecraft.txt'
     # loads a dictionary of spacecraft TLEs and other relevant information
     satellites = load.tle(fl)
     satList = satellites.values()
     satList = list(set(satList))
     satList.sort(key=sortNames)
-    d = timeNow.utcnow()
-    return (satList,d,spacecraftNames)
+    satList = np.asarray(satList)
+    return (satList)
 
 
-
-def getNewData(timeNow):
+def getNewData():
     # gets new data from space-track.org if it has been longer than a day
     # that TLE data has been requested
     fl = 'time.txt'
@@ -97,10 +100,7 @@ def getNewData(timeNow):
     user = input("spacetrack username: ")
     pd = input("password: ")
     st = SpaceTrackClient(user, pd)
-    timeOld = datetime(timeNow.year, timeNow.month, timeNow.day, 0, 7, 35) + timedelta(days=1)
-    timeOld += timedelta(seconds=.3333333)
-    with open(fl, 'w') as file:
-        file.write(str(timeOld))
+
     # norad_cat_id=[op.inclusive_range(1,36050)] instead of norad_cat_id=[25544, 42712], etc.
     data = st.tle_latest(norad_cat_id=IDs, ordinal=1, format='json')
     a = json.loads(data)
@@ -114,117 +114,60 @@ def getNewData(timeNow):
             b = craft['TLE_LINE0'] + '\n'
             file.write(b)
             name = b[2:]
-            spacecraftNames.append(name)
             b = craft['TLE_LINE1'] + '\n'
             file.write(b)
             b = craft['TLE_LINE2'] + '\n'
             file.write(b)
-    spacecraftNames.sort()
-    fl = 'names.txt'
-    with open(fl, 'w') as file:
-        for name in spacecraftNames:
-            file.write(name)
-    return spacecraftNames
 
-def writeSpacecraftTitle():
-    fl = "satelliteData.csv"
-    with open(fl, 'w') as file:
-        file.write('name,')
-        file.write('latitude,')
-        file.write('longitude,')
-        file.write('elevation,')
-        file.write('year,')
-        file.write('month,')
-        file.write('day,')
-        file.write('hour,')
-        file.write('minute,')
-        file.write('second,')
+
+
+def getOrbitData(sat):
+    pos = np.array(sat.at(t).position.km)/10
+    alt = [(((sat - x).at(t)).altaz()) for x in siteLocations]
+    #alt = (alt[0][0].degrees > 0)*1
+    #print(len(alt))
+    tt = t[iter].utc_datetime()
+    with open(fl, 'a') as f:
+        for i in range(len(pos[0])):
+            f.write(sat.name + ',')
+            for j in range(len(pos)):
+                f.write(str(pos[j][i]) + ',')
+            f.write(str(tt.year) + ',')
+            f.write(str(tt.month) + ',')
+            f.write(str(tt.day) + ',')
+            f.write(str(tt.hour) + ',')
+            f.write(str(tt.minute) + ',')
+            f.write(str(tt.second) + ',')
+            for j in range(len(alt)):
+                a = (alt[j][0].degrees > 0)*1
+                f.write(str(a[i]) + ',')
+            f.write('\n')
+
+
+def writeTitle(fl):
+    with open(fl,'w') as f:
+        f.write('name,')
+        f.write('x,')
+        f.write('y,')
+        f.write('z,')
+        f.write('year,')
+        f.write('month,')
+        f.write('day,')
+        f.write('hour,')
+        f.write('minute,')
+        f.write('second,')
         for i in range(len(siteLocations)):
-            file.write('h' + str((i + 1)) + ',')
-        file.write('\n')
-
-def writeGSData(oLength):
-    fl = "groundData.csv"
-    with open(fl, 'w') as file:
-        file.write('name,')
-        file.write('lat,')
-        file.write('lon,\n')
-        for i in range(len(siteLocations)):
-            file.write(siteNames[i] + ',')
-            file.write(str(siteLocations[i].latitude.degrees) + ',')
-            file.write(str(siteLocations[i].longitude.degrees) + ',')
-            file.write('\n')
-    fl = "orbitLength.csv"
-    with open(fl, 'w') as file:
-        file.write("len,\n")
-        file.write(str(oLength))
-
-
-    # calculates the positions of the spacecraft during the list of times in t
-    # to get an orbit
-def getOrbitData(satellite, t):
-    print(satellite.name)
-    # https://rhodesmill.org/brandon/2018/tiangong/
-
-    # gets the spacecraft position at time t, which is a list in this case
-    geocentric = satellite.at(t)
-    # gets the longitude latitude of the point on the earth below the spacecraft,
-    # and the spacecraft's elevation above the earth at time t
-    subpoint = geocentric.subpoint()
-    lat = subpoint.latitude.degrees
-    long = subpoint.longitude.degrees
-    el = subpoint.elevation.km
-    horizonData = []
-    for i in siteLocations:
-        orbit = (satellite - i).at(t)
-        alt, az, dist = orbit.altaz()
-        horizonData.append((alt.degrees > 0).astype(int))
-
-
-    # gets data about the spacecraft and its orbit and writes it to a csv file
-    fl = "satelliteData.csv"
-    with open(fl, 'a') as file:
-        for i in range(len(lat)):
-            file.write(satellite.name + ',')
-            file.write(str(lat[i]) + ',')
-            file.write(str(long[i]) + ',')
-            file.write(str(el[i]) + ',')
-            timeStripped = str(t[i].utc)[1:-1]
-            file.write(timeStripped + ',')
-            for j in horizonData:
-                file.write(str(int(j[i])) + ',')
-            file.write('\n')
-
-def getSunPos(t):
-    planets = load('de421.bsp')
-    earth, sun = planets['earth'], planets['sun']
-    point = earth + Topos('90 N', '0 E', None, None, -6378136)
-    fl = 'sun.csv'
-    with open(fl, 'w') as file:
-        file.write('lat,')
-        file.write('lon,\n')
-        for tTime in t:
-            alt, az, dis = point.at(tTime).observe(sun).apparent().altaz()
-            az = 180 - az.degrees
-            file.write(str(alt.degrees) + ',')
-            file.write(str(az) + ',\n')
+            f.write('h' + str(i+1) + ',')
+        f.write('\n')
 
 def main():
-    ts = load.timescale()
-    m = 60
-    hr = 24
-    minutes = range(m * hr)
-    writeSpacecraftTitle()
-    writeGSData(m * hr)
-    satellites, d, spacecraftNames = compareTime()
-    print(satellites)
-    print(len(satellites))
-    t = ts.utc(d.year, d.month, d.day, d.hour + (d.minute / 60), minutes)
-    getSunPos(t)
-
-    #satellite = satellites[craft]  # [:-1]
-    for craft in satellites:
-        getOrbitData(craft, t)
+    satList = compareTime(d)
+    fl = 'satelliteData.csv'
+    writeTitle(fl)
+    v = np.vectorize(getOrbitData)
+    v(satList)
+    e = datetime.utcnow() - d
+    print(e)
 
 if __name__ == "__main__":
     main()
